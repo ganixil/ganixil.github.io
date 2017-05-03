2e27e5a40f7c77c94427fce1794808569e2a1d6f
xiangli <xiangli1997@gmail.com>
2017-05-02 20:19:25 -0400

    Personal Website

---
+++ helper.js
@@ -0,0 +1,18 @@
+    function openCover() {
+      window.open('source/docs/cover.pdf');
+    }
+    function openResume(){
+      window.open('source/docs/Resume.pdf');
+    }
+    function openUser(){
+      window.open('source/docs/user.pdf');
+    }
+    function openLit(){
+      window.open('source/docs/lit.pdf');
+    }
+    function openResearch(){
+      window.open('source/docs/research.pdf');
+    }
+    function openNews(){
+      window.open('source/news.pdf');
+    }
